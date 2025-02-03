d236990f2d45fa21ba9d7e34d0a077a9483f2c56
Ryley Watson <ryleyswatson@gmail.com>
2025-02-03 09:53:33 -0800

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target['new-task-description'].value);
+  });
 });
+
+function handleToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  li.textContent = todo;  
+  li.appendChild(btn);
+  btn.addEventListener('click', () => {
+    li.remove();
+  });
+  document.querySelector('#tasks').appendChild(li); 
+}
