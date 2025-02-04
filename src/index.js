0d01f5f4adf53d52792b7a32459269896d5135bb
Ryley Watson <ryleyswatson@gmail.com>
2025-02-04 10:43:35 -0800

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    handleToDo(e.target['new-task-description'].value);
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#create-task-form');
+  const formInput = document.querySelector('#new-task-description');
+  const taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const task = formInput.value.trim();
+    if (task) {
+      const li = document.createElement('li');
+      li.textContent = task;
+      taskList.appendChild(li);
+      formInput.value = '';
+    }
   });
 });
-
-function handleToDo(todo) {
-  let li = document.createElement('li');
-  let btn = document.createElement('button');
-  btn.textContent = 'x';
-  li.textContent = todo;  
-  li.appendChild(btn);
-  btn.addEventListener('click', () => {
-    li.remove();
-  });
-  document.querySelector('#tasks').appendChild(li); 
-}
